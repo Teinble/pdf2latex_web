3a1e6b2e6696faab9798d9d898fc0a112961173d
Teinble <xilingzhao16@gmail.com>
2025-10-13 14:33:52 -0400

    refactor: revert the repo to original set up

--- frontend/react-router.config.ts
+++ frontend/react-router.config.ts
@@ -1,9 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-	// Config options...
-	// Server-side render by default, to enable SPA mode set this to `false`
-	appDirectory: "src/app",
-	serverModuleFormat: "esm",
-	ssr: true,
+  // Config options...
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: true,
 } satisfies Config;
