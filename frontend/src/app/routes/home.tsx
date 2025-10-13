5166aabd868ddfadd3e8c849e8cb7e2449b92de8
Teinble <xilingzhao16@gmail.com>
2025-10-13 14:40:54 -0400

    del: init to hello world

--- frontend/src/app/routes/home.tsx
+++ frontend/src/app/routes/home.tsx
@@ -1,13 +1,3 @@
-import { Welcome } from "../welcome/welcome";
-import type { Route } from "./+types/home";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
 export default function Home() {
-  return <Welcome />;
+  return <p>Hello World</p>;
 }
